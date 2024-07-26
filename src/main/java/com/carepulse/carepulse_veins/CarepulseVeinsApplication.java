abebfaf3c4fc00b5b8ef8844d2d437fb0af5291f
Andy Chang <yuhaoz1991@gmail.com>
2024-07-25 21:21:14 -0700

    Springboot project init

---
+++ src/main/java/com/carepulse/carepulse_veins/CarepulseVeinsApplication.java
@@ -0,0 +1,13 @@
+package com.carepulse.carepulse_veins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarepulseVeinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarepulseVeinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carepulse/carepulse_veins/CarepulseVeinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carepulse.carepulse_veins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarepulseVeinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
