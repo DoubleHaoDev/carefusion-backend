8023d37237b26270983b0d4b0ac0a3f5f020b92f
Andy Chang <yuhaoz1991@gmail.com>
2024-07-26 20:17:34 -0700

    Add formatter and logger

--- src/test/java/com/carepulse/carepulse_veins/CarepulseVeinsApplicationTests.java
+++ src/test/java/com/carepulse/carepulse_veins/CarepulseVeinsApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CarepulseVeinsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
