d0adf73548da1f89aab002cdc70b4455d9e9c303
Andy Chang <105964806+DoubleHaoDev@users.noreply.github.com>
2024-08-18 20:03:16 -0700

    Add userUuid in the response of login endpoint (#8)

--- src/main/java/com/carefusion/carefusion_backend/model/dto/response/LoginResponse.java
+++ src/main/java/com/carefusion/carefusion_backend/model/dto/response/LoginResponse.java
@@ -1,14 +1,17 @@
 package com.carefusion.carefusion_backend.model.dto.response;
 
+import java.util.UUID;
+
 public class LoginResponse {
   private String token;
 
   private long expiresIn;
 
+  private UUID userUuid;
+
   public String getToken() {
     return token;
   }
-
 
   public void setToken(String token) {
     this.token = token;
@@ -20,5 +23,13 @@ public class LoginResponse {
 
   public void setExpiresIn(long expiresIn) {
     this.expiresIn = expiresIn;
+  }
+
+  public UUID getUserUuid() {
+    return userUuid;
+  }
+
+  public void setUserUuid(UUID userUuid) {
+    this.userUuid = userUuid;
   }
 }
